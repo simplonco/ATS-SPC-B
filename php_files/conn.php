d6138479713451e178fe1072ea3588634009c3fc
Yvan Sraka <yvan.sraka@gmail.com>
2016-08-17 12:24:11 +0200

    Clean PHP code with Atom Beautify (Ctrl + Alt + B)

--- php_files/conn.php
+++ php_files/conn.php
@@ -1,12 +1,12 @@
 <?php
-$host="localhost";
-$db_user="root";
-$db_password="simplonco";
-$db_name="ATC_SPC";
 
-try{
-	$conn=new pdo("mysql:host=$host;dbname=$db_name",$db_user,$db_password);
-}
-catch (PDOException $e){
-	echo "not connected :". $e->getMessage();
+$host = 'localhost';
+$db_user = 'root';
+$db_password = 'simplonco';
+$db_name = 'ATC_SPC';
+
+try {
+    $conn = new pdo("mysql:host=$host;dbname=$db_name", $db_user, $db_password);
+} catch (PDOException $e) {
+    echo 'not connected :'.$e->getMessage();
 }
