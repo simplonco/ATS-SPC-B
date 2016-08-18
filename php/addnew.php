e895af5ba7244f9a1a1ec5d34337e236399d7392
aboualnaser <aboualnaser@gmail.com>
2016-08-18 11:49:34 +0200

    first commite

--- php/addnew.php
+++ php/addnew.php
@@ -2,78 +2,107 @@
 <html>
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
-    <link href="../css/custom.css" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="../css/bootstrap.min.css" rel="stylesheet">
+  <link href="../css/custom.css" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
 </head>
-
 <body>
-    <?php
-    if (isset($_POST['add-new'])) {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        $email = $_POST['email'];
-        $errors = array();
-        if (empty($username) || empty($password)) {
-            $errors[] = 'ALL Fields requierd';
-        } else {
-            include 'conn.php';
-            $sql = 'INSERT INTO users(name,passcode) VALUES (?,?)';
-            $stmt = $conn->prepare($sql);
-            $stmt->bindParam(1, $username, PDO::PARAM_STR);
-            $stmt->bindParam(2, $password, PDO::PARAM_STR);
-            $stmt->execute();
-            $success = 'the table is add';
-        }
-    }
-    ?>
-    <div id="page">
-        <div id="page_header">
-            <h1>Projet SPC</h1>
-            <h2>(Suivi de Présence du Collaborateur)</h2>
-        </div>
-        <ul class="nav nav-pills">
-            <li role="presentation" class="active"><a href="dashbord.php"><span class="glyphicon glyphicon-asterisk"></span>Dashbord</a></li>
-            <li role="presentation"><a href="addnew.php"><span class="glyphicon glyphicon-asterisk"></span>Addnew</a></li>
-            <li role="presentation"><a href="showall.php"><span class="glyphicon glyphicon-asterisk"></span>Report</a></li>
-            <li role="presentation"><a href="login.php"><span class="glyphicon glyphicon-asterisk"></span>login</a></li>
-        </ul>
-        <form class="form-horizontal" action="" method="POST">
-            <br />
-            <?php
-            if ($success) {
-                echo '<div class="alert alert-success">'.$success.'</div>';
-            }
-            if (isset($errors)) {
-                foreach ($errors as $error) {
-                    echo '<div class="alert alert-danger">'.$error.'</div>';
-                }
-            }
-            ?>
-            <div class="control-group">
-                <label class="control-label" for="textinput-1">username</label>
-                <div class="controls">
-                    <input id="textinput-1" name="username" type="text" placeholder="username" class="input-xlarge">
-                </div>
-            </div>
-            <!-- Text input-->
-            <div class="control-group">
-                <label class="control-label" for="textinput-1">password</label>
-                <div class="controls">
-                    <input id="textinput-1" name="password" type="text" placeholder="password" class="password">
-                </div>
-            </div>
-            <!-- Button -->
-            <div class="control-group">
-                <div class="controls">
-                    <button id="singlebutton-0" name="add-new" class="btn btn-primary">Add</button>
-                </div>
-            </div>
-        </form>
-    </div>
-</body>
+  <?php
+	if(isset($_POST['add-new'])){
+		$username=$_POST["name"];
+    $surname=$_POST["surname"];
+		$password=$_POST["passcode"];
+		$mail=$_POST["mail"];
 
+        $errors =array();
+		if (empty($username) || empty($password)) {
+			$errors[]="ALL Fields requierd";
+		}
+		else{
+		include 'conn.php';
+		$sql ="INSERT INTO users(name,surname,mail,passcode) VALUES (?,?,?,?)";
+		$stmt=$conn->prepare($sql);
+		$stmt->bindParam(1,$username,PDO::PARAM_STR);
+		$stmt->bindParam(2,$surname,PDO::PARAM_STR);
+    $stmt->bindParam(3,$mail,PDO::PARAM_STR);
+    $stmt->bindParam(4,$password,PDO::PARAM_STR);
+		$stmt->execute();
+		$success = "the table is add";
+		}
+	}
+?>
+  <div id="page">
+  <div id="page_header">
+    <h1>Projet SPC</h1>
+<h2>(Suivi de Présence du Collaborateur)</h2>
+</div>
+<ul class="nav nav-pills">
+  <li role="presentation" class="active"><a href="dashbord.php"><span class="glyphicon glyphicon-asterisk"></span>Dashbord</a></li>
+  <li role="presentation"><a href="addnew.php"><span class="glyphicon glyphicon-asterisk"></span>Addnew</a></li>
+  <li role="presentation"><a href="showall.php"><span class="glyphicon glyphicon-asterisk"></span>Report</a></li>
+  <li role="presentation"><a href="login.php"><span class="glyphicon glyphicon-asterisk"></span>login</a></li>
+</ul>
+<form class="form-horizontal" action="" method="POST">
+  <br/>
+  <?php
+  if($success){
+  	echo '<div class="alert alert-success">'.$success.'</div>';
+  }
+  if(isset($errors))
+  {
+  	foreach ($errors as $error) {
+  		echo '<div class="alert alert-danger">'.$error.'</div>';
+  	}
+  }
+  ?>
+
+
+  <div class="control-group">
+    <label class="control-label" for="textinput-1">username</label>
+    <div class="controls">
+      <input id="textinput-1" name="name" type="text" placeholder="username" class="input-xlarge">
+    </div>
+  </div>
+
+  <!-- Text input-->
+  <div class="control-group">
+    <label class="control-label" for="textinput-1">surname</label>
+    <div class="controls">
+      <input id="textinput-1" name="surname" type="text" placeholder="surname" class="input-xlarge">
+    </div>
+  </div>
+  <!-- Text input-->
+  <div class="control-group">
+    <label class="control-label" for="textinput-1">email</label>
+    <div class="controls">
+      <input id="textinput-1" name="mail" type="text" placeholder="mail" class="input-xlarge">
+    </div>
+  </div>
+  <!-- Text input-->
+  <div class="control-group">
+    <label class="control-label" for="textinput-1">passcode</label>
+    <div class="controls">
+      <input id="textinput-1" name="passcode" type="text" placeholder="passcode" class="input-xlarge">
+    </div>
+  </div>
+<!-- Button -->
+<div class="control-group">
+  <div class="controls">
+    <button id="singlebutton-0" name="add-new" class="btn btn-primary">Add</button>
+  </div>
+</div>
+</fieldset>
+</form>
+
+
+
+
+
+  </div>
+
+</body>
 </html>
--- php/dashbord.php
+++ php/dashbord.php
@@ -2,97 +2,101 @@
 <html>
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
-    <link href="../css/custom.css" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="../css/bootstrap.min.css" rel="stylesheet">
+  <link href="../css/custom.css" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
 </head>
-
 <body>
-    <div id="page">
-        <div id="page_header">
-            <h1>Projet SPC</h1>
-            <h2>(Suivi de Présence du Collaborateur)</h2>
-        </div>
-        <ul class="nav nav-pills">
-            <li role="presentation" class="active"><a href="dashbord.php"><span class="glyphicon glyphicon-asterisk"></span>Dashbord</a></li>
-            <li role="presentation"><a href="addnew.php"><span class="glyphicon glyphicon-asterisk"></span>Addnew</a></li>
-            <li role="presentation"><a href="showall.php"><span class="glyphicon glyphicon-asterisk"></span>Report</a></li>
-            <li role="presentation"><a href="login.php"><span class="glyphicon glyphicon-asterisk"></span>login</a></li>
-        </ul>
-        <?php
-        error_reporting('E_ALL & ~E_NOTIC');
-        require 'conn.php';
-        if ($_GET['login'] == 'activ') {
-            $id = intval($_GET['id']);
-            $sql = 'UPDATE login SET activ=1 WHERE id=:id ';
-            $stml = $conn->prepare($sql);
-            $stml->bindParam(':id', $id, PDO::PARAM_INT);
-            $stml->execute();
-            header('location:dashbord.php');
-        } elseif ($_GET['login'] == 'unactiv') {
-            $id = intval($_GET['id']);
-            $sql = 'UPDATE login SET activ=0 WHERE id=:id ';
-            $stml = $conn->prepare($sql);
-            $stml->bindParam(':id', $id, PDO::PARAM_INT);
-            $stml->execute();
-            header('location:dashbord.php');
-        }
-        $sql = 'SELECT * FROM login ORDER BY id';
-        $stmt = $conn->query($sql);
-        $count = $stmt->rowcount();
-        $id = 1;
-        if ($count) {
-        ?>
-        <table class="table">
-            <tr>
-                <td>id</td>
-                <td>user name</td>
-                <td>password</td>
-                <td>time</time></td>
-                <td>activ</td>
-            </tr>
-            <?php
-            while ($row = $stmt->fetch(PDO::FETCH_OBJ)) {
-                if ($row->activ == 0) {
-                    echo'
-                    <tr>
-                    <td>'.$id++."</td>
-                    <td>{$row->username}</td>
-                    <td>{$row->password}</td>
-                    <td>{$row->Time}</td>
-                    <td>{$row->activ}</td>
-                    <td><a href ='dashbord.php?login=activ&id={$row->id}'>activ</a></td>
-                            <td>{$row->timd}</td>
-                    </tr>
-                    ";
-                } else if ($row->activ == 1) {
-                    $date = "$row->Time";
-                    $date = strtotime($date);
-                    $date = date('H', $date);
-                    echo'
-                    <tr>
-                    <td>'.$id++."</td>
-                    <td>{$row->username}</td>
-                    <td>{$row->password}</td>
-                    <td>{$row->Time}</td>
-                    <td>{$row->activ}</td>
-                    <td>$date</td>
-                    <td><a href ='dashbord.php?login=unactiv&id={$row->id}'>unactiv</a></td>
-                    </tr>
-                    ";
-                }
-            }
-        ?>
-        </table>
-        <?php
-            } else {
-                // TODO: Display an error!
-            }
-        ?>
-    </div>
-</body>
+  <div id="page">
+  <div id="page_header">
+    <h1>Projet SPC</h1>
+<h2>(Suivi de Présence du Collaborateur)</h2>
+</div>
+<ul class="nav nav-pills">
+  <li role="presentation" class="active"><a href="dashbord.php"><span class="glyphicon glyphicon-asterisk"></span>Dashbord</a></li>
+  <li role="presentation"><a href="addnew.php"><span class="glyphicon glyphicon-asterisk"></span>Addnew</a></li>
+  <li role="presentation"><a href="showall.php"><span class="glyphicon glyphicon-asterisk"></span>Report</a></li>
+  <li role="presentation"><a href="login.php"><span class="glyphicon glyphicon-asterisk"></span>login</a></li>
+</ul>
+<?php
+error_reporting("E_ALL & ~E_NOTIC");
+require 'conn.php';
+if ($_GET['users']=='activ') {
+	$id = intval($_GET['id']);
+	$sql="UPDATE users SET activ=1 WHERE id=:id ";
+	$stml=$conn->prepare($sql);
+	$stml->bindParam(':id',$id,PDO::PARAM_INT);
+	$stml->execute();
+	header("location:dashbord.php");
+}
+elseif ($_GET['users']=='unactiv') {
+	$id = intval($_GET['id']);
+	$sql="UPDATE users SET activ=0 WHERE id=:id ";
+	$stml=$conn->prepare($sql);
+	$stml->bindParam(':id',$id,PDO::PARAM_INT);
+	$stml->execute();
+	header("location:dashbord.php");
+}
+$sql="SELECT * FROM users ORDER BY id";
+$stmt =$conn->query($sql);
+$count=$stmt->rowcount();
+$id=1;
+if($count){
 
+
+?>
+<table class="table">
+	<tr>
+  <td>id</td>
+  <td>name</td>
+  <td>surname</td>
+  <td>email</time></td>
+  <td>passcode</td>
+</tr>
+<?php
+while ( $row=$stmt->fetch(PDO::FETCH_OBJ)) {
+	if ($row->activ ==0) {
+		echo"
+		<tr>
+		<td>".$id++."</td>
+		<td>{$row->name}</td>
+		<td>{$row->surname}</td>
+		<td>{$row->mail}</td>
+    <td>{$row->passcode}</td>
+		<td><a href ='dashbord.php?login=activ&id={$row->id}'>activ</a></td>
+        <td>{$row->timd}</td>
+		</tr>
+		";
+	}
+	elseif($row->activ==1){
+    $date = "$row->Time";
+  $date = strtotime($date);
+  $date= date('H', $date);
+echo"
+		<tr>
+		<td>".$id++."</td>
+		<td>{$row->name}</td>
+		<td>{$row->surname}</td>
+    <td>{$row->mail}</td>
+		<td>{$row->passcode}</td>
+    <td>$date</td>
+		<td><a href ='dashbord.php?login=unactiv&id={$row->id}'>unactiv</a></td>
+		</tr>
+		";
+	}
+}
+
+?>
+</table>
+<?php
+}
+else{}
+?>
+  </div>
+
+</body>
 </html>
