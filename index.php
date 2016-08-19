bd66f6f1f9689c0be8641222302cae8b3f385591
adora84 <abdulkader.kalaaji@gmail.com>
2016-08-19 17:47:09 +0200

    Fix all bugs, and finish the project, and leave Simplon to enjoy my weekend

--- index.php
+++ index.php
@@ -11,132 +11,100 @@
 </head>
 
 <body>
-<?php
-if(isset($_POST['Validate'])){
-    $passcode = $_POST["passcode"];
-    $errors = array();
-    if (empty($passcode)) {
-        $errors[] = "All fields required";
-    } else {
-
-        // Connect to database
-        include 'php/conn.php';
-
-        // Retreive user form passcode
-        $sql_user = "SELECT * FROM users WHERE passcode = $passcode";
-        $stmt_user = $conn->query($sql_user);
-        $user_id = 0;
-        while($row_user = $stmt_user->fetch(PDO::FETCH_ASSOC)) {
-          $user_id = $row_user["id"];
-        }
-
-        if ($user_id == 0) {
-          $errors[] = "The user id is wrong";
-        } else {
-
-          // Add entry in checkin table
-          // TODO: Check if there is already an entry of this user today
-          $sql_checkin ="INSERT INTO checkins(user_id) VALUES (?)";
-          $stmt_checkin = $conn->prepare($sql_checkin);
-          $stmt_checkin->bindParam(1, $user_id, PDO::PARAM_STR);
-          $stmt_checkin->execute();
-
-          // Retreive arrival time
-          $sql_arrival_time = "SELECT arrival_time FROM checkins WHERE user_id = $user_id ORDER BY id";
-          $stmt_arrival_time = $conn->query($sql_arrival_time);
-
-          while($row = $stmt_arrival_time->fetch(PDO::FETCH_ASSOC)) {
-              $ARRIVAL_TIME = $row["arrival_time"];
-          }
-
-          // ** DEBUG ** //
-          echo "<br /><h1>ARRIVAL_TIME = ".$ARRIVAL_TIME."</h1><br />";
-          // *********** //
-          //$ARRIVAL_TIME = new DateTime($data['current_time']);
-          $ARRIVAL_TIME=".$ARRIVAL_TIME.";
-          $LATE_TIME = new DateTime('10:00:00');
-          $ABSENT_TIME = new DateTime('12:00:00');
-            if ($ARRIVAL_TIME  > $LATE_TIME && $ARRIVAL_TIME < $ABSENT_TIME) {
-                $errors[] = "Welcome but you are late for today";
-            } else if ($ARRIVAL_TIME > $ABSENT_TIME) {
-                $errors[] = "Welcome but You will be considered as absent today".$ARRIVAL_TIME;
-            } else {
-                $success = "Welcome and Thank you<br />".$ARRIVAL_TIME;
-            }
-        }
-    }
-}
-?>
-<div id="page">
-  <div id="page_header">
-    <h1>Projet SPC</h1>
-    <h2>(Suivi de Présence du Collaborateur)</h2>
-  </div>
-
-  <ul class="nav nav-pills">
-    <li role="presentation" class="active"><a href="php/dashbord.php"><span class="glyphicon glyphicon-asterisk"></span>Dashbord</a></li>
-    <li role="presentation"class="active"><a href="php/addnew.php"><span class="glyphicon glyphicon-asterisk"></span>Addnew</a></li>
-    <li role="presentation"class="active"><a href="php/showall.php"><span class="glyphicon glyphicon-asterisk"></span>Showall</a></li>
-  </ul>
-
-  <form class="form-horizontal" action="" method="POST">
-    <br />
-    <?php
-      if(isset($_POST['Validate'])){
-        $passcode = $_POST["passcode"];
-        $errors = array();
-        if (empty($passcode)) {
-          $errors[] = "ALL Fields requierd";
-        } else {
+  <?php
+  if (isset($_POST['Validate'])) {
+      $passcode = $_POST["passcode"];
+      $errors = array();
+      if (empty($passcode)) {
+          $errors[] = "All fields required";
+      } else {
+          // Connect to database
           include 'php/conn.php';
-          $sql = "SELECT * FROM users WHERE passcode = $passcode";
-          $stmt = $conn->query($sql);
+
+          // Retreive user form passcode
+          $sql_user = "SELECT * FROM users WHERE passcode = $passcode";
+          $stmt_user = $conn->query($sql_user);
           $user_id = 0;
-          while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            $user_id = $row["id"];
+          while($row_user = $stmt_user->fetch(PDO::FETCH_ASSOC)) {
+            $user_id = $row_user["id"];
           }
+
           if ($user_id == 0) {
             $errors[] = "The user id is wrong";
           } else {
-            $sqle ="INSERT INTO checkins(user_id) VALUES (?)";
-        		$stmt=$conn->prepare($sqle);
-        		$stmt->bindParam(1,$user_id,PDO::PARAM_STR);
-        		$stmt->execute();
-            //$success = "the user id is ".$user_id;
-          }
-        }
-      }
-    ?>
 
-          <br />
-          <?php
-          if ($success) {
-            echo '<div class="alert alert-success">'.$success.'</div>';
-          }
-          if (isset($errors)) {
-            foreach ($errors as $error) {
-              echo '<div class="alert alert-danger">'.$error.'</div>';
+            // Add entry in checkin table
+            // TODO: Check if there is already an entry of this user today
+            $sql_checkin ="INSERT INTO checkins(user_id) VALUES (?)";
+            $stmt_checkin = $conn->prepare($sql_checkin);
+            $stmt_checkin->bindParam(1, $user_id, PDO::PARAM_STR);
+            $stmt_checkin->execute();
+
+            // Retreive arrival time
+            $sql_arrival_time = "SELECT arrival_time FROM checkins WHERE user_id = $user_id ORDER BY id";
+            $stmt_arrival_time = $conn->query($sql_arrival_time);
+
+            while($row = $stmt_arrival_time->fetch(PDO::FETCH_ASSOC)) {
+                $ARRIVAL_TIME = new DateTime($row["arrival_time"]);
+            }
+
+            $LATE_TIME = new DateTime('10:00:00');
+            $ABSENT_TIME = new DateTime('12:00:00');
+
+            if ($ARRIVAL_TIME  > $LATE_TIME && $ARRIVAL_TIME < $ABSENT_TIME) {
+                $errors[] = "Welcome but you are late for today:<br />".$ARRIVAL_TIME->format('Y-m-d H:i:s');
+            } else if ($ARRIVAL_TIME > $ABSENT_TIME) {
+                $errors[] = "Welcome but You will be considered as absent today:<br />".$ARRIVAL_TIME->format('Y-m-d H:i:s');
+            } else {
+                $success = "Welcome and Thank you:<br />".$ARRIVAL_TIME->format('Y-m-d H:i:s');
             }
           }
-          ?>
-          <div class="control-group">
-            <ul class="nav nav-pills">
-                <li role="presentation"class="active"><a href="index.php"><span class="glyphicon glyphicon-asterisk"></span>checkin</a></li>
-            </ul>
-            <label class="control-label" for="textinput-1">passcode</label>
-            <div class="controls">
-              <input id="textinput-1" name="passcode" type="text" placeholder="passcode" class="input-xlarge">
-            </div>
-          </div>
+      }
+  }
+  ?>
 
-          <!-- Button -->
-          <div class="control-group">
-            <div class="controls">
-              <button id="singlebutton-0" name="Validate" class="btn btn-primary" onclick="newDoc()">Validate</button>
-            </div>
-          </div>
-        </form>
-
+  <div id="page">
+    <div id="page_header">
+      <h1>Projet SPC</h1>
+      <h2>(Suivi de Présence du Collaborateur)</h2>
     </div>
-  </body>
+
+    <ul class="nav nav-pills">
+      <li role="presentation" class="active"><a href="php/dashbord.php"><span class="glyphicon glyphicon-asterisk"></span>Dashbord</a></li>
+      <li role="presentation"class="active"><a href="php/addnew.php"><span class="glyphicon glyphicon-asterisk"></span>Addnew</a></li>
+      <li role="presentation"class="active"><a href="php/showall.php"><span class="glyphicon glyphicon-asterisk"></span>Showall</a></li>
+      <li role="presentation"class="active"><a href="index.php"><span class="glyphicon glyphicon-asterisk"></span>checkin</a></li>
+    </ul>
+
+    <form class="form-horizontal" action="" method="POST">
+      <br />
+      <?php
+      if ($success) {
+        echo '<div class="alert alert-success">'.$success.'</div>';
+      }
+      if (isset($errors)) {
+        foreach ($errors as $error) {
+          echo '<div class="alert alert-danger">'.$error.'</div>';
+        }
+      }
+      ?>
+      <div class="control-group">
+        <label class="control-label" for="textinput-1">passcode</label>
+        <div class="controls">
+          <input id="textinput-1" name="passcode" type="text" placeholder="passcode" class="input-xlarge">
+        </div>
+      </div>
+      <br />
+
+      <!-- Button -->
+      <div class="control-group">
+        <div class="controls">
+          <button id="singlebutton-0" name="Validate" class="btn btn-primary" onclick="newDoc()">Validate</button>
+        </div>
+      </div>
+    </form>
+
+  </div>
+</body>
+
 </html>
