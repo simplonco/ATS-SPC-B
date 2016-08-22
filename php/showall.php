a7de7a5cfdefed416fe04ae4d3353b170c8fe6f1
adora84 <abdulkader.kalaaji@gmail.com>
2016-08-22 11:03:36 +0200

    remove duplicated code and create one file

--- php/showall.php
+++ php/showall.php
@@ -1,14 +1,7 @@
 <!DOCTYPE html>
 <html>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
-    <link href="../css/custom.css" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-</head>
+<?php include '../header.inc.php'; ?>
 
 <body>
     <div id="page">
